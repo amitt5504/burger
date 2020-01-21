e7e66ac161eb72ed57e70a008e2c2e28dd1ab928
Aaron J Mitchell <amitchell1992@knights.ucf.edu>
2020-01-21 18:19:39 -0500

    working

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Mushroom & Swiss');
+INSERT INTO burgers (burger_name) VALUES ('Bacon & Cheddar');
+INSERT INTO burgers (burger_name) VALUES ('Jalepeno Pepper Jack');
